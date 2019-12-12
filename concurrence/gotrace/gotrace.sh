719f0a5564e8399622d5a151d3b354c10e5b1ce4
newbmiao <newbvirgil@gmail.com>
2019-12-12 21:01:17 +0800

    slim gotrace

--- concurrence/gotrace/gotrace.sh
+++ concurrence/gotrace/gotrace.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 
-DOCKERPATH="https://gist.githubusercontent.com/NewbMiao/f4340b483e3dfc057911cba8e7a37562/raw/4383fff99051ecd5a09bec4c8bd7cac0b46a57ae/Dockerfile"
-GOTRACECMD="docker run --rm -it  -p 2000:2000  -v \$PWD:/go divan/golang:gotrace1.8"
+DOCKERPATH="https://gist.githubusercontent.com/NewbMiao/f4340b483e3dfc057911cba8e7a37562/raw/5e88cbf6edb16af3d78162a69917591e274df8af/Dockerfile"
+IMAGETAG="divan/golang:gotrace1.8"
 
 if [ $# -eq 0 ]; then
     echo -e "Usage: Require go file pathname(some code enable trace goroutines)\n\
-    Shell : sh gotrace.sh goroutines.go\n\
-    Docker: $GOTRACECMD goroutines.go\n\
-            (you need build image locally first: docker build . -t divan/golang:gotrace1.8)"
+    Shell : sh gotrace.sh goroutines.go"
     exit;
 fi
 
-if [ -f /.dockerenv ]; then # build on docker container
-    gotrace $1
-else # build on local machine
-    exist=$(docker images|grep divan/golang |grep gotrace1.8|wc -l)
-    if [ $exist -eq 0 ]; then
-        echo "build image divan/golang:gotrace1.8"
-        {
-            workspace=$(cd $(dirname $0) && pwd -P)
-            cd $workspace
-            if [ ! -f "Dockerfile" ]; then 
-                curl -o Dockerfile "$DOCKERPATH"
-            fi
-            docker build . -t  divan/golang:gotrace1.8
-        }
-    fi
-    eval $GOTRACECMD $1
+exist=$(docker images|grep divan/golang |grep gotrace1.8|wc -l)
+if [ $exist -eq 0 ]; then
+    echo "build image $IMAGETAG"
+    {
+        workspace=$(cd $(dirname $0) && pwd -P)
+        cd $workspace
+        if [ ! -f "Dockerfile" ]; then 
+            curl -o Dockerfile "$DOCKERPATH"
+        fi
+        docker build . -t  $IMAGETAG
+    }
 fi
+
+docker run --rm -it  -p 2000:2000  -v $PWD:/go $IMAGETAG $1
