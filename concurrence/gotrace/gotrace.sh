9617d62fb6e5d806ff52cdba8a03757ed3b5dc02
newbmiao <newbvirgil@gmail.com>
2019-12-12 13:10:45 +0800

    update docker link

--- concurrence/gotrace/gotrace.sh
+++ concurrence/gotrace/gotrace.sh
@@ -7,7 +7,7 @@ if [ $exist -eq 0 ]; then
         workspace=$(cd $(dirname $0) && pwd -P)
         cd $workspace
         if [ ! -f "Dockerfile" ]; then 
-            curl -o Dockerfile "https://gist.githubusercontent.com/NewbMiao/f4340b483e3dfc057911cba8e7a37562/raw/9e97aa713ac7be673a5f938c1e57bd5fcfad8af3/Dockerfile_gotrace1.8" 
+            curl -o Dockerfile "https://gist.githubusercontent.com/NewbMiao/f4340b483e3dfc057911cba8e7a37562/raw/7b8ab53511af85f614d3354b2da40ec9c21c5e76/Dockerfile"
         fi
         docker build . -t  divan/golang:gotrace1.8
     }
